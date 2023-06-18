66e859fe34ced4929845eb4dcf4e9e02cde3f492
Mateus B. Marques <mateusbiegermarques@gmail.com>
2023-06-18 19:02:00 -0300

    tabelas

---
+++ init.sql
@@ -0,0 +1,3 @@
+INSERT INTO cargos(Nome) VALUES ('Administrador');
+INSERT INTO funcionarios(Nome, SobreNome, Telefone, Cpf, Salario, Ativo, Permissao, Usuario, Senha) 
+VALUES ('Administrador', 'Geral', '00000000000', '00000000000', 0, 1, 3, 'admin', 'admin');
